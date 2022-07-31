833d901cb6b75ca415eeaa2afc61fa9d8b2d5a26
Islomjons <mukhammadalievislomjon@gmail.com>
2022-07-31 17:02:56 +0500

    sasa

--- js/main.js
+++ js/main.js
@@ -2,30 +2,62 @@ let moviesArray = movies.slice(0, 50);
 
 let elBtn = document.querySelector("#btn__form");
 let elInput = document.querySelector("#input__form");
+let elSpan = document.querySelector(".result__number");
+let elSelectCategory = document.querySelector(".select__category")
 let elMovieWrapper = document.querySelector(".movie__wrapper");
 let elMoviesCard = document.querySelector("#movies__card").content;
 
-function normalize(array) {
-    let newArray = [];
-    array.forEach(item => {
-        let newObject = {};
-        newObject.title = item.Title.toString();
-        newObject.videoUrl = `https://www.youtube.com/watch?v=${item.ytid}`;
-        newObject.categories = item.Categories.split("|")
-        newObject.movieYear = item.movie_year;
-        newObject.imdbRating = item.imdb_rating;
-        newObject.img = `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`;
-        newArray.push(newObject)
-    });
-    return newArray
+
+let newArray = moviesArray.map(function(item) {
+    return {
+        title: item.Title.toString(),
+        videoUrl: `https://www.youtube.com/watch?v=${item.ytid}`,
+        categories: item.Categories.split("|"),
+        movieYear: item.movie_year,
+        imdbRating: item.imdb_rating,
+        img: `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`,
+    }
+});
+
+
+function getCategory(array) {
+
+    let categoriesArray = [];
+
+    for (const item of newArray) {
+        for (const itemCategory of item.categories) {
+            if(!categoriesArray.includes(itemCategory)){
+                categoriesArray.push(itemCategory)
+            }
+        }
+    }
+
+    return categoriesArray
 }
 
+let moviesCategory = getCategory(newArray);
+function renderCatagory(array, notion) {
+    notion.innerHTML = null;
+    
+    let tempFragment = document.createDocumentFragment();
 
-let newArray = normalize(moviesArray);
+    let newOption = document.createElement("option");
+    for (const item of array) {
+        let newOption = document.createElement("option");
+        newOption.textContent = item;
+        newOption.value = item
+        
+        tempFragment.appendChild(newOption)
+    }
+    notion.appendChild(tempFragment)
+}
+
+renderCatagory(moviesCategory, elSelectCategory)
+
 
 function renderMovies(array, wrapper) {
-    wrapper.innerHTML = null
-    
+    wrapper.innerHTML = null;
+    elSpan.textContent = array.length
     let tempFragment = document.createDocumentFragment();
     for (let item of array) {
             let templateItem = elMoviesCard.cloneNode(true)
@@ -42,6 +74,25 @@ function renderMovies(array, wrapper) {
 }
 
 renderMovies(newArray, elMovieWrapper);
+
+elSelectCategory.addEventListener("input", function(event){
+    event.preventDefault();
+
+    let selectCategory = elSelectCategory.value;
+    
+    let filteredArray = newArray.filter(function (item) {
+        return item.categories.includes(selectCategory);
+    })
+
+    if(selectCategory !="all") {
+        renderMovies(filteredArray, elMovieWrapper)
+    }else{
+        renderMovies(newArray, elMovieWrapper)
+    }
+})
+
+
+
 
 let elForm = document.querySelector("#form");
 elForm.addEventListener("submit", function(event){
